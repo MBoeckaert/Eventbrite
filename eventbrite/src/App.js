d4aa45304c87f23f5ea5415b3670157ef25255c3
Matthias <matthiasboeckaert@gmail.com>
2022-07-10 14:57:39 +0200

    clean up default react app + create pages

--- eventbrite/src/App.js
+++ eventbrite/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Likes from "./pages/Likes";
+import Profile from "./pages/Profile.js";
+import Search from "./pages/Search.js";
+import Tickets from "./pages/Tickets.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home></Home>
+      <Likes></Likes>
+      <Profile></Profile>
+      <Search></Search>
+      <Tickets></Tickets>
     </div>
   );
 }
---
+++ eventbrite/src/pages/Likes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Likes = () => {
+  return <h1>Likes</h1>;
+};
+
+export default Likes;
