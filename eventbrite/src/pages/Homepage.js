98927f38746232eee64b53b9a61fd0dacf463510
Matthias <matthiasboeckaert@gmail.com>
2022-07-14 20:43:55 +0200

    check if there are events close to you with dummy__events on homepage.js

--- eventbrite/src/pages/Homepage.js
+++ eventbrite/src/pages/Homepage.js
@@ -1,21 +1,30 @@
 import EventOverview from "../components/EventOverview";
 
 const Homepage = (props) => {
+  /* Check if there are events available close to you with a ternary operator */
+  let amountEventsAvailable = <p>There are no events close to you</p>;
+
+  if (props.events.length > 0) {
+    amountEventsAvailable = props.events.map((event) => (
+      <EventOverview
+        key={event.id}
+        img={event.img}
+        name={event.name}
+        date={event.date}
+        location={event.location}
+      />
+    ));
+  }
+
   return (
     <>
+      {/* use a filter or work with geolocation API */}
       <p>Evenementen zoeken in</p>
       <h1>Oost-Vlaanderen</h1>
 
       {/* Popular in Oost-Vl niet doen */}
-      {props.events.map((event) => (
-        <EventOverview
-          key={event.id}
-          img={event.img}
-          name={event.name}
-          date={event.date}
-          location={event.location}
-        />
-      ))}
+
+      {amountEventsAvailable}
     </>
   );
 };
