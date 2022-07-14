903026dc1cd2adcf41cd968e3b4b42c19850cfdf
Matthias <matthiasboeckaert@gmail.com>
2022-07-14 20:26:38 +0200

    EventOverview component can now get rendered dynamically in homepage.js

--- eventbrite/src/App.js
+++ eventbrite/src/App.js
@@ -5,9 +5,32 @@ import EventInformation from "./pages/EventInformation.js";
 import PayTickets from "./pages/PayTickets";
 
 const App = () => {
+  const dummy__events = [
+    {
+      id: "e1",
+      img: "IMG",
+      date: new Date("2022", "09", "11"),
+      name: "First Event Name",
+      location: "Ghent",
+    },
+    {
+      id: "e2",
+      img: "IMG2",
+      date: new Date("2022", "07", "1"),
+      name: "Second Event Name",
+      location: "Deinze",
+    },
+    {
+      id: "e3",
+      img: "IMG3",
+      date: new Date("2022", "08", "21"),
+      name: "Third Event Name",
+      location: "Lede",
+    },
+  ];
   return (
     <div className="App">
-      <Homepage></Homepage>
+      <Homepage events={dummy__events}></Homepage>
       <EventInformation></EventInformation>
       <PayTickets></PayTickets>
     </div>
--- eventbrite/src/pages/Homepage.js
+++ eventbrite/src/pages/Homepage.js
@@ -1,42 +1,21 @@
 import EventOverview from "../components/EventOverview";
 
-const Homepage = () => {
-  const events = [
-    {
-      id: "e1",
-      img: "IMG",
-      date: new Date("2022", "09", "11"),
-      name: "First Event Name",
-      location: "Ghent",
-    },
-    {
-      id: "e2",
-      img: "IMG2",
-      date: new Date("2022", "07", "1"),
-      name: "Second Event Name",
-      location: "Deinze",
-    },
-    {
-      id: "e3",
-      img: "IMG3",
-      date: new Date("2022", "08", "21"),
-      name: "Third Event Name",
-      location: "Lede",
-    },
-  ];
-
+const Homepage = (props) => {
   return (
     <>
       <p>Evenementen zoeken in</p>
       <h1>Oost-Vlaanderen</h1>
 
       {/* Popular in Oost-Vl niet doen */}
-      <EventOverview
-        img={events[1].img}
-        name={events[1].name}
-        date={events[1].date}
-        location={events[1].location}
-      />
+      {props.events.map((event) => (
+        <EventOverview
+          key={event.id}
+          img={event.img}
+          name={event.name}
+          date={event.date}
+          location={event.location}
+        />
+      ))}
     </>
   );
 };
